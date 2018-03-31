1efbcbbd7be8798344b1628a6053e2cbc90dcc02
tatsumi kentaro <tatsuken4290@gmail.com>
2018-03-31 14:46:46 +0900

    暫定版

---
+++ controllmemusample/Event/EventDetailViewController.swift
@@ -0,0 +1,124 @@
+//
+//  EventDetailViewController.swift
+//  controllmemusample
+//
+//  Created by tatsumi kentaro on 2018/03/31.
+//  Copyright © 2018年 tatsumi kentaro. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class EventDetailViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var chatPostTextField: UITextField!
+    
+    
+    @IBOutlet weak var mainTable: UITableView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var eventDetail: UILabel!
+    @IBOutlet weak var postName: UILabel!
+    
+    var event: Event! 
+    var realtimeDB: DatabaseReference!
+    var db: Firestore!
+    var name : String!
+    var profilePath : StorageReference!
+    var contentsArray = [String: String]()
+    var getArray = [String]()
+    var getMainArray = [[String]]()
+    var myname: String!
+    
+    @IBOutlet weak var labelView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTable.delegate = self
+        mainTable.dataSource = self
+        labelView.layer.borderColor = UIColor.orange.cgColor
+        labelView.layer.borderWidth = 1.0
+        labelView.layer.cornerRadius = 5.0
+        labelView.layer.masksToBounds = true
+        db = Firestore.firestore()
+        if let uid = Auth.auth().currentUser?.uid{
+            db.collection("users").document(uid).getDocument(completion: { (snap, error) in
+                if let error = error{
+                    print(error.localizedDescription)
+                }else{
+                    let data = snap?.data()
+                    self.myname = data!["name"] as! String
+                }
+            })
+        }
+        eventName.text = event.eventTitle
+        eventDate.text = event.eventDate
+        eventDetail.text = event.evetDetail
+        postName.text = event.postUserName
+        profilePath.downloadURL { url, error in
+            if let error = error {
+                print(error.localizedDescription)
+                // Handle any errors
+            } else {
+                //imageViewに描画、SDWebImageライブラリを使用して描画
+                self.imageView.sd_setImage(with: url!, completed: nil)
+                
+            }
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        realtimeDB = Database.database().reference()
+        realtimeDB.ref.child("event").child("message").child(event.EventID).observe(.value) { (snap) in
+            self.getMainArray = [[String]]()
+            for item in snap.children {
+                //ここは非常にハマるfirebaseはjson形式なので変換が必要
+                let child = item as! DataSnapshot
+                let dic = child.value as! NSDictionary
+                self.getArray = [dic["name"]! as! String, dic["contents"]! as! String]
+                self.getMainArray.append(self.getArray)
+            }
+            print(self.getMainArray)
+            self.mainTable.reloadData()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func post(_ sender: UIButton) {
+        
+        guard chatPostTextField.text != "" else {
+            print("何か入力してください")
+            return
+        }
+        contentsArray = ["name": myname,"contents": chatPostTextField.text!]
+        realtimeDB = Database.database().reference()
+        realtimeDB.ref.child("event").child("message").child(event.EventID).childByAutoId().setValue(contentsArray)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ 
+        return getMainArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        if tableView.tag == 2{
+            let nameLabel = cell.contentView.viewWithTag(1) as! UILabel
+            let contentsLabel = cell.contentView.viewWithTag(2) as! UILabel
+            nameLabel.text = getMainArray[indexPath.row][0]
+            contentsLabel.text = getMainArray[indexPath.row][1]
+            
+        }
+        
+        return cell
+    }
+    
+    
+}
---
+++ controllmemusample/evaluation/SearchListViewController.swift
@@ -0,0 +1,212 @@
+//
+//  SearchListViewController.swift
+//  controllmemusample
+//
+//  Created by tatsumi kentaro on 2018/03/09.
+//  Copyright © 2018年 tatsumi kentaro. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SearchListViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet var mainTable: UITableView!
+    var tagArray = [[String]]()
+    var db: Firestore!
+    var evaluationArray = [Evaluation]()
+    var numOfcell: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTable.dataSource = self
+        mainTable.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        evaluationArray = [Evaluation]()
+        print(tagArray.count)
+        print(tagArray)
+        db = Firestore.firestore()
+        switch tagArray.count {
+        case 1:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                        self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+                        print(self.evaluationArray)
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+            
+        case 2:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                              self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 3:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                               self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 4:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                                self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+                 }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 5:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").whereField("\(tagArray[4][0])", isEqualTo: "\(tagArray[4][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                     self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 6:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").whereField("\(tagArray[4][0])", isEqualTo: "\(tagArray[4][1])").whereField("\(tagArray[5][0])", isEqualTo: "\(tagArray[5][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                                  self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 7:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").whereField("\(tagArray[4][0])", isEqualTo: "\(tagArray[4][1])").whereField("\(tagArray[5][0])", isEqualTo: "\(tagArray[5][1])").whereField("\(tagArray[6][0])", isEqualTo: "\(tagArray[6][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                                 self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 8:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").whereField("\(tagArray[4][0])", isEqualTo: "\(tagArray[4][1])").whereField("\(tagArray[5][0])", isEqualTo: "\(tagArray[5][1])").whereField("\(tagArray[6][0])", isEqualTo: "\(tagArray[6][1])").whereField("\(tagArray[7][0])", isEqualTo: "\(tagArray[7][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                                self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 9:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").whereField("\(tagArray[4][0])", isEqualTo: "\(tagArray[4][1])").whereField("\(tagArray[5][0])", isEqualTo: "\(tagArray[5][1])").whereField("\(tagArray[6][0])", isEqualTo: "\(tagArray[6][1])").whereField("\(tagArray[7][0])", isEqualTo: "\(tagArray[7][1])").whereField("\(tagArray[8][0])", isEqualTo: "\(tagArray[8][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                                   self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+        case 10:
+            db.collection("courseEvaluation").whereField("\(tagArray[0][0])", isEqualTo: "\(tagArray[0][1])").whereField("\(tagArray[1][0])", isEqualTo: "\(tagArray[1][1])").whereField("\(tagArray[2][0])", isEqualTo: "\(tagArray[2][1])").whereField("\(tagArray[3][0])", isEqualTo: "\(tagArray[3][1])").whereField("\(tagArray[4][0])", isEqualTo: "\(tagArray[4][1])").whereField("\(tagArray[5][0])", isEqualTo: "\(tagArray[5][1])").whereField("\(tagArray[6][0])", isEqualTo: "\(tagArray[6][1])").whereField("\(tagArray[7][0])", isEqualTo: "\(tagArray[7][1])").whereField("\(tagArray[8][0])", isEqualTo: "\(tagArray[8][1])").whereField("\(tagArray[9][0])", isEqualTo: "\(tagArray[9][1])").getDocuments(completion: { (snap, error) in
+                if let error = error{
+                    print("\(error)")
+                }else{
+                    for document in (snap?.documents)!{
+                        let data = document.data()
+                           self.evaluationArray.append(Evaluation(className: data["courseName"] as! String, teacherName: data["teacherName"] as! String, course: data["course"] as! String, year: data["year"] as! String, attendance: data["attendance"] as! String, textbook: data["textbook"] as! String, courseEvaluation: data["courseEvaluation"] as! String, different: data["different"] as! String, coursedetail: data["courseDetail"] as! String, postuid: document.documentID, middleExamination: data["middleExamination"] as! String, finalExamination: data["finalExamination"] as! String))
+
+                    }
+                    self.mainTable.reloadData()
+                }
+            })
+            
+            
+        default:
+            return
+        }
+        print(evaluationArray)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return evaluationArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let courseName = cell.contentView.viewWithTag(1) as! UILabel
+        let teacherName = cell.contentView.viewWithTag(2) as! UILabel
+        let year = cell.contentView.viewWithTag(3) as! UILabel
+        let course = cell.contentView.viewWithTag(4) as! UILabel
+        let courseEvaluation = cell.contentView.viewWithTag(5) as! UILabel
+        let different = cell.contentView.viewWithTag(6) as! UILabel
+        
+        courseName.text = evaluationArray[indexPath.row].className
+        teacherName.text = evaluationArray[indexPath.row].teacherName
+        year.text = evaluationArray[indexPath.row].year
+        course.text = evaluationArray[indexPath.row].course
+        courseEvaluation.text = evaluationArray[indexPath.row].courseEvaluation
+        courseEvaluation.textColor = UIColor.orange
+        different.text = evaluationArray[indexPath.row].different
+        different.textColor = UIColor.orange
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        numOfcell = indexPath.row
+        performSegue(withIdentifier: "goDetail", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goDetail"{
+            let saerchListViewController = segue.destination as! SearchDeatilViewController
+            saerchListViewController.evaluation = self.evaluationArray[numOfcell]
+            
+        }
+    }
+    
+}
